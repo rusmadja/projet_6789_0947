cc79307aa9f9357558266f9f307ef8ec827e0490
reouven smadja <rusmadja@g.jct.ac.il>
2020-03-31 11:49:15 +0300

    ajout spehere test

--- unitTest/geometries/SphereTest.java
+++ unitTest/geometries/SphereTest.java
@@ -1,12 +1,32 @@
 package geometries;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import primitives.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SphereTest extends Polygon {
 
     @Test
-    public void testGetNormal() {
+    public void getNormalTest() {
+        Sphere s1 = new Sphere(4, new Point3D(0,0,0));
+        Sphere s2 = new Sphere(1, new primitives.Point3D(1,1,1));
+
+
+        assertTrue(s1.getNormal(new Point3D(0,0,4)).equals(new Vector(new Point3D(0,0,1))));
+        assertTrue(s1.getNormal(new Point3D(0,0,-4)).equals(new Vector(new Point3D(0,0,-1))));
+        assertTrue(s1.getNormal(new Point3D(0,4,0)).equals(new Vector(new Point3D(0,1,0))));
+        assertTrue(s1.getNormal(new Point3D(0,-4,0)).equals(new Vector(new Point3D(0,-1,0))));
+        assertTrue(s1.getNormal(new Point3D(4,0,0)).equals(new Vector(new Point3D(1,0,0))));
+        assertTrue(s1.getNormal(new Point3D(-4,0,0)).equals(new Vector(new Point3D(-1,0,0))));
+
+        assertTrue(s2.getNormal(new Point3D(1,1,0)).equals(new Vector(new Point3D(0,0,-1))));
+        assertTrue(s2.getNormal(new Point3D(0,1,1)).equals(new Vector(new Point3D(-1,0,0))));
+        assertTrue(s2.getNormal(new Point3D(1,0,1)).equals(new Vector(new Point3D(0,-1,0))));
+
+        assertEquals(s1.getNormal(new Point3D(7,7,7)),null);
+
     }
 }
+
+
