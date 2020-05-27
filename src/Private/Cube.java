3c90df71f06a0fffe39498b11f7184b3dd2f0bbb
reouven smadja <rusmadja@g.jct.ac.il>
2020-05-27 23:23:28 +0300

    notre version pour nous et que pour nous

---
+++ src/Private/Cube.java
@@ -0,0 +1,113 @@
+package Private;
+
+import elements.Material;
+import geometries.Geometries;
+import geometries.Intersectable;
+import geometries.Polygon;
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cube extends Geometries
+{
+    Polygon Polygon_bottom ;
+    Polygon Polygon_up ;
+    Polygon Polygon_down ;
+    Polygon Polygon_left_side ;
+    Polygon Polygon_right_side ;
+    Polygon Polygon_front ;
+
+    public Cube(Color emissionLight, Material material, Point3D position, int longueur, int hauteur, int profondeur)
+    {
+        double x = position.get_x().get();
+        double y = position.get_y().get();
+        double z = position.get_z().get();
+
+
+        Polygon_bottom = new Polygon(
+                emissionLight,
+                material,
+                new Point3D(x, y, z+profondeur),
+                new Point3D(x+longueur, y, z+profondeur),
+                new Point3D(x+longueur, y+hauteur, z+profondeur),
+                new Point3D(x, y+hauteur, z+profondeur)
+        );
+        Polygon_front      = new Polygon(
+                emissionLight,
+                material,
+                new Point3D(x, y, z),
+                new Point3D(x+longueur, y, z),
+                new Point3D(x+longueur, y+hauteur, z),
+                new Point3D(x, y+hauteur, z)
+        );
+        Polygon_up         = new Polygon(
+                emissionLight,
+                material,
+                new Point3D( x,y+hauteur,z),
+                new Point3D(x+longueur, y+hauteur, z),
+                new Point3D(x+longueur, y+hauteur, z+longueur),
+                new Point3D(x, y+hauteur, z+profondeur)
+        );
+        Polygon_down       = new Polygon(
+                emissionLight,
+                material,
+                new Point3D( x,y,z),
+                new Point3D(x+longueur, y, z),
+                new Point3D(x+longueur, y, z+longueur),
+                new Point3D(x, y, z+profondeur)
+        );
+
+        Polygon_left_side  = new Polygon(
+                emissionLight,
+                material,
+                new Point3D(x, y, z),
+                new Point3D(x, y, z+profondeur),
+                new Point3D(x, y+hauteur, z+profondeur),
+                new Point3D(x, y+hauteur, z)
+        );
+
+        Polygon_right_side = new Polygon(
+                emissionLight,
+                material,
+                new Point3D(x+longueur, y, z),
+                new Point3D(x+longueur, y, z+profondeur),
+                new Point3D(x+longueur, y+hauteur, z+profondeur),
+                new Point3D(x+longueur, y+hauteur, z)
+        );
+
+    }
+
+    public Vector getNormal(Point3D p) {
+        return null;
+    }
+
+    public List<GeoPoint> findIntersections(Ray ray, double MAXDISTANCE) {
+
+        List<Intersectable.GeoPoint> intersections = new LinkedList<GeoPoint>();
+
+        List<GeoPoint> GeoPoint_Bottom = Polygon_bottom.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_Bottom != null ) intersections.add(GeoPoint_Bottom.get(0));
+
+        List<GeoPoint> GeoPoint_front = Polygon_front.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_front != null ) intersections.add(GeoPoint_front.get(0));
+
+        List<GeoPoint> GeoPoint_up= Polygon_up.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_up != null ) intersections.add(GeoPoint_up.get(0));
+
+        List<GeoPoint> GeoPoint_down = Polygon_down.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_down != null ) intersections.add(GeoPoint_down.get(0));
+
+        List<GeoPoint> GeoPoint_left_side = Polygon_left_side.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_left_side != null ) intersections.add(GeoPoint_left_side.get(0));
+
+        List<GeoPoint> GeoPoint_right_side = Polygon_right_side.findIntersections(ray,MAXDISTANCE);
+        if(GeoPoint_right_side != null ) intersections.add(GeoPoint_right_side.get(0));
+
+
+        return intersections;
+    }
+}
---
+++ unitTest/Private/CubeTest.java
@@ -0,0 +1,119 @@
+package Private;
+
+import elements.AmbientLight;
+import elements.Camera;
+import elements.Material;
+import elements.SpotLight;
+import geometries.Intersectable.GeoPoint;
+import geometries.Triangle;
+import org.junit.Test;
+import primitives.Color;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+import renderer.ImageWriter;
+import renderer.Render;
+import scene.Scene;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CubeTest {
+
+    @Test
+    public void testConstructor() {
+        try{
+            Cube test = new Cube(
+                    new Color(0, 0, 200),
+                    new Material(0, 0, 1),
+                    new Point3D(0, 0, 0), 4, 4, 4);
+
+
+        } catch (IllegalArgumentException e) {
+
+        System.out.println(e);}
+
+    }
+
+
+    @Test
+    public void TestfindIntersections() {
+        Cube test = new Cube(
+                new Color(0, 0, 200),
+                new Material(0, 0, 1),
+                new Point3D(0, 0, 0), 4, 4, 4);
+        Ray ray = new Ray(new Point3D(2,2,-2),new Vector(0,0,1)) ;
+
+        List<GeoPoint> TesterList = test.findIntersections(ray, Double.POSITIVE_INFINITY);
+
+        assertEquals( "bad number intersection",TesterList.size() , 2 );
+
+        assertEquals("bad number intersection",
+                List.of( TesterList.get(0).getPoint(),TesterList.get(1).getPoint()) ,
+                List.of(new Point3D(2,2,4), new Point3D(2,2,0)));
+    }
+
+
+    @Test
+    public void test1_1() {
+        Scene scene = new Scene.SceneBuilder("Test scene")
+                .addCamera(
+                        new Camera(
+                                new Point3D(100, 2, -1000),
+                                new Vector(0, 0, 3),
+                                new Vector(-1, -1, 0)))
+                .addDistance(1000)
+                .addAmbientLight(new AmbientLight(new Color(java.awt.Color.BLUE), 0.15))
+                .addBackground(Color.BLACK)
+                .build();
+        scene.addGeometries(
+                new Cube(
+                        new Color(200,200,100),
+                        new Material(0.5, 0.5, 60 , 1,0),
+                        new Point3D(0,0,0),
+                        50,50,100)
+        );
+        scene.addLights(new SpotLight(new Color(700, 400, 400), //
+                new Point3D(60, -50, 0), new Vector(0, 0, 1), 1, 4E-5, 2E-7));
+
+        ImageWriter imageWriter = new ImageWriter("test1.0", 200, 200, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.writeToImage();
+    }
+    @Test
+    public void test1_2() {
+        Scene scene;
+        scene = new Scene.SceneBuilder("Test scene")
+                .addAmbientLight(new AmbientLight(Color.BLACK, 0))
+                .addCamera(new Camera(
+                        new Point3D(50, 50, -1000),
+                        new Vector(0, 0, 0.5),
+                        new Vector(0.5, -1, 0)))
+                .addDistance(1000)
+                .addBackground(Color.BLACK)
+                .build();
+
+        scene.addGeometries( //
+                new Triangle(Color.BLACK, new Material(0, 0.8, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
+                new Triangle(Color.BLACK, new Material(0, 0.8, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 140), new Point3D(75, -75, 150)), //
+                new Cube(new Color(java.awt.Color.BLUE),
+                        new Material(0.5, 0.5, 30),
+                        new Point3D(0, 0, 115),
+                        50,50,50));
+
+        scene.addLights(new SpotLight(new Color(700, 400, 400), //
+                new Point3D(40, -40, -115), new Vector(-1, 1, 4), 1, 4E-4, 2E-5));
+
+        ImageWriter imageWriter = new ImageWriter("test1.2", 200, 200, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.writeToImage();
+    }
+}
